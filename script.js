a9d7e072ee4a49f52b470ac83105fe3795fd785d
Mannu1811 <88558308+Mannu1811@users.noreply.github.com>
2023-06-05 03:15:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var url = 'https://www.filltext.com/?rows=32&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D';
+var url = 'http://www.filltext.com/?rows=32&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D';
 
 let tableElem = document.querySelector('#table-data');
 let tbodyElem = tableElem.querySelector('tbody');
@@ -6,12 +6,23 @@ let rowInd = tbodyElem.querySelector('tr.data-row')
 tbodyElem.replaceChildren();
 
 let user;
-fetch(url)
-.then(resp => resp.json())
-.then(data => {
-	user = data;
-	loadUser();
-})
+// fetch(url)
+// .then(resp => resp.json())
+// .then(data => {
+// 	user = data;
+// 	loadUser();
+// })
+
+function fetchUrl() {
+	fetch(url)
+	.then(resp => resp.json())
+	.then(data => {
+		user = data;
+		loadUser();
+	})
+}
+
+setTimeout(fetchUrl, 500);
 
 function showDetail() {	
 	for(let i=0; i<user.length; i++) {
